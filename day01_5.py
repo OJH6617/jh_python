fd1ad3f28dad80cd659de473b38e6dc85e28396d
OJH6617 <wngjs24865@gmail.com>
2023-01-08 10:05:45 +0900

    juheon's python

---
+++ day01_5.py
@@ -0,0 +1,14 @@
+#정수형태의 변수는 연산이 가능함
+
+ #사과의 가격을 구하기
+
+ #price, count
+price = int(input('사과의 가격:'))
+print('사과의 가격은',price,'원 입니다.')
+
+count = int(input('사과의 갯수:'))
+print('사과의 갯수는',count,'개 입니다')
+
+print('총 가격은',price * count,'원 입니다')
+
+
